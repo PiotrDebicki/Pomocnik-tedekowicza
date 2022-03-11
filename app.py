fb406b8e693e934b697cee8b7e52277f5aba12ef
PiotrDebicki <piotr@debicki.wroclaw.pl>
2022-03-11 22:08:22 +0100

    Added calculator for transform curves

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ bootstrap = Bootstrap(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+#Strona /krzywe-przejsciowe
+@app.route('/krzywe-przejsciowe', methods=['GET', 'POST'])
+def krzywe_przejsciowe():
+    return render_template('krzywe-przejsciowe.html')
